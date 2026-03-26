a99a425434e855c5da3f0d199e9b382fcbaefacb
DSM5 <fatec-dsm5@academico.intra>
2026-03-25 21:35:48 -0300

    :sparkles: feat: configuração dos 2 cruds: times e jogadores

--- server/src/main/kotlin/com/fatec/att_lddm_ktor/repository/FootballRepository.kt
+++ server/src/main/kotlin/com/fatec/att_lddm_ktor/repository/FootballRepository.kt
@@ -1,12 +1,64 @@
 package com.fatec.att_lddm_ktor.repository
 
-import com.fatec.att_lddm_ktor.database.*
-import com.fatec.att_lddm_ktor.database.DatabaseFactory.dbQuery
+import com.fatec.att_lddm_ktor.database.TeamsTable
+import com.fatec.att_lddm_ktor.database.PlayersTable
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.transaction
 
 class FootballRepository {
-    // Busca todos os times do banco
-    suspend fun allTeams(): List<String> = dbQuery {
-        TeamsTable.selectAll().map { it[TeamsTable.nome] }
+
+    fun allTeams(): List<Map<String, Any>> = transaction {
+        TeamsTable.selectAll().map {
+            mapOf(
+                "id" to it[TeamsTable.id],
+                "nome" to it[TeamsTable.nome],
+                "estadio" to it[TeamsTable.estadio],
+                "cidade" to it[TeamsTable.cidade]
+            )
+        }
+    }
+
+    fun createTeam(nomeT: String, estadioT: String, cidadeT: String) = transaction {
+        TeamsTable.insert {
+            it[nome] = nomeT
+            it[estadio] = estadioT
+            it[cidade] = cidadeT
+        }
+    }
+
+    fun updateTeam(idT: Int, nomeT: String, estadioT: String, cidadeT: String) = transaction {
+        TeamsTable.update({ TeamsTable.id eq idT }) {
+            it[nome] = nomeT
+            it[estadio] = estadioT
+            it[cidade] = cidadeT
+        }
+    }
+
+    fun deleteTeam(idT: Int) = transaction {
+        TeamsTable.deleteWhere { TeamsTable.id eq idT }
+    }
+
+    fun allPlayers(): List<Map<String, Any>> = transaction {
+        PlayersTable.selectAll().map {
+            mapOf(
+                "id" to it[PlayersTable.id],
+                "nome" to it[PlayersTable.nome],
+                "posicao" to it[PlayersTable.posicao],
+                "teamId" to it[PlayersTable.teamId]
+            )
+        }
+    }
+
+    fun createPlayer(nomeP: String, posicaoP: String, idDoTime: Int) = transaction {
+        PlayersTable.insert {
+            it[nome] = nomeP
+            it[posicao] = posicaoP
+            it[teamId] = idDoTime
+        }
+    }
+
+    fun deletePlayer(idP: Int) = transaction {
+        PlayersTable.deleteWhere { PlayersTable.id eq idP }
     }
 }
